61871751bf797119e40be5bd14a247c7c6c6ccdd
EthanLam <ethan__lam@163.com>
2018-03-24 23:37:09 +0800

    upd

--- passenger/pages/index/index.js
+++ passenger/pages/index/index.js
@@ -3,7 +3,13 @@
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
