46036cb36bd97c93fe45121a99ed08a0d6505646
EthanLam <ethan__lam@163.com>
2018-03-26 00:34:23 +0800

    upd

--- passenger/pages/air/index/index.js
+++ passenger/pages/air/index/index.js
@@ -5,9 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    banner: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    airplan: [{
+      name: '墨尔本',
+      id: 1212
+    }, {
+      name: '悉尼',
+      id: 1213
+    }],
+    index: 0
   },
-
+  bindPickerChange (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
