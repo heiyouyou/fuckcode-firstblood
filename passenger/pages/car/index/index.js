5a83e4a1942cd17ee2519bcbf6d39c730e222fda
EthanLam <ethan__lam@163.com>
2018-04-07 14:35:47 +0800

    upd

--- passenger/pages/car/index/index.js
+++ passenger/pages/car/index/index.js
@@ -1,18 +1,33 @@
-// pages/car/index/index.js
+// pages/reserve/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    price:　{
+      rmb: 28,
+      usd: 2.8  
+    }
+    
   },
-
+  getSysInfo() {
+    wx.getSystemInfo({
+      success: res => {
+        this.setData({
+          height: res.windowHeight
+        })
+        console.log(res)
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getSysInfo()
   },
 
   /**
