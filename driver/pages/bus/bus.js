57651c60050b974263446e84e6b4187412754d9b
Wells <1337238043@qq.com>
2018-04-27 20:32:38 +0800

    add

--- driver/pages/bus/bus.js
+++ driver/pages/bus/bus.js
@@ -5,9 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    time:'',
+    date:''
   },
+  bindTimeChange(e){
+    this.setData({
+      time:e.detail.value
+    })
+  },
+  bindDateChange(e){
+    this.setData({
+      date:e.detail.value
+    })
+  },
+  createBus(){
+    wx.showModal({
+      title: '确认创建班车',
+      content: '确认创建您的班车吗？ \n创建后您可以手动开关您的班车',
+      confirmColor:'#F1604F',
+      cancelColor:'#8F8E94',
+      success: function(res) {
+        if (res.confirm) {
 
+        } else if (res.cancel) {
+          
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
